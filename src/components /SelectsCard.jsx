506da02048be4ad897dd5def01dbff439465bfeb
comd6 <101353952+comd6@users.noreply.github.com>
2022-08-19 15:03:33 +0300

    1

---
+++ src/components /SelectsCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SelectsCard = (props) => {
+  return (
+    <div className="relative">
+      <img
+        className="w-full h-full object-cover rounded-md"
+        src={props.bg}
+        alt="/"
+      />
+      <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full rounded-md">
+        <p className="left-4 bottom-4 text-2xl font-bold absolute text-white">
+          {props.text}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SelectsCard;
